8506ce8cbcd8eeedbd8fca64ca0b92625e69b044
Joyce Lin <joycelin79@gmail.com>
2019-01-04 14:00:37 -0800

    update folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../img/logo.svg';
+import '../css/App.css';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './css/reset.css';
+import './css/index.css';
+import App from './components/App';
+import * as serviceWorker from './components/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
