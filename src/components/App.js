02db2f04a4a5f79ee2248905d593710e5ee07255
Joyce Lin <joycelin79@gmail.com>
2019-01-04 16:01:56 -0800

    add UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.svg';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="nav">
+          <a href="/" className="nav-brand">catURL</a>
+        </div>
+
+        <a href="https://github.com/loopDelicious/cat-kube">
+          <img className="snipe" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" />
+        </a>
+
+        <div className="guts">
+
+          <div className="message-contents">
+            <form id="encoder">
+              <div className="submit-encoder">
+                <h3>enter an original url to create a custom cat url: </h3>
+                <input type="text" id="notes" className="encode-text" value="https://www.google.com" />
+                <input type="submit" className="btn submit-btn" value="cattify" />
+              </div>
+            </form>
+          </div>
+
+          <div className="display-contents">
+              <div className='display-intro'>
+                  <h3>is now: </h3>
+              </div>
+
+              <div className="display">
+                  <input type="text" className="new-url" readonly />
+                  <input type="button" className="btn copy-btn" value="copy" data-clipboard-action="copy" data-clipboard-target=".new-url" />
+              </div>
+          </div>
+
+          <div id="attribution">created by: <a href='http://www.github.com/loopDelicious'>Joyce Lin</a></div>
+          <div class='disclaimer'>
+            <p>Intended for entertainment use only. No guarantees made. No one endorses anything contained in the URLs. 
+              <div> {"\n"}{"\n"} </div>
+              CatURL is not responsible for any content linked through its service. Use at your own risk.  It's a litter bit amazing.
+            </p>
+          </div>
+        
+        </div>
+
       </div>
     );
   }
